8afe9566126cc457f8e7375a56a580fc2dd903d0
Dinesh B <dineshb@winjit.com>
2021-06-20 09:29:07 +0530

    App center configuration changes are merged

--- BluetoothLeChat/BluetoothLeChat/App.xaml.cs
+++ BluetoothLeChat/BluetoothLeChat/App.xaml.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Xamarin.Forms.Internals;
+using BluetoothLeChat.Constants;
+using Microsoft.AppCenter.Distribute;
 
 namespace BluetoothLeChat
 {
+    [Preserve(AllMembers = true)]
     public partial class App : Application
     {
+        #region Constructor
+
         public App()
         {
             InitializeComponent();
@@ -13,16 +21,31 @@ namespace BluetoothLeChat
             MainPage = new MainPage();
         }
 
+        #endregion
+
+        #region LifeCycle Overrides
+
         protected override void OnStart()
         {
+            AppCenter.Configure("android=" + AppKeyConstants.AppCenter_Android_SecretKey +
+                "ios=" + AppKeyConstants.AppCenter_iOS_SecretKey);
+            if (AppCenter.Configured)
+            {
+                AppCenter.Start(typeof(Analytics));
+                AppCenter.Start(typeof(Crashes));
+                AppCenter.Start(typeof(Distribute));
+            }
         }
 
         protected override void OnSleep()
         {
+
         }
 
         protected override void OnResume()
         {
         }
+
+        #endregion
     }
 }
---
+++ BluetoothLeChat/BluetoothLeChat/Constants/AppKeyConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xamarin.Forms.Internals;
+
+namespace BluetoothLeChat.Constants
+{
+    [Preserve(AllMembers = true)]
+    public static class AppKeyConstants
+    {
+        public const string AppCenter_Android_SecretKey = "8529bf79-0acb-41ef-a8ed-14f204bfb499;";
+        public const string AppCenter_iOS_SecretKey = "c6b41ce6-4e47-4676-9749-18dab502f608;";
+    }
+}
